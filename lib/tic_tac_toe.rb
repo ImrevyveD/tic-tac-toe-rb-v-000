df7b516c147bfc3242617f2913595e6a26c7f8f4
Divya Rao <divya1aum@gmail.com>
2018-01-23 21:57:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,3 +9,52 @@ WIN_COMBINATIONS = [
   [2,4,6],
   [2,5,8]
 ]
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+
+
